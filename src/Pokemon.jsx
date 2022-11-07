0b80d959caf1b70472e033cac8195fc66a038473
bubakazh <troymanansala@gmail.com>
2022-11-07 02:28:48 -0800

    p to span, to save space

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -11,6 +11,7 @@ const Pokemon = () => {
         axios.get("https://pokeapi.co/api/v2/pokemon/?limit=905")
             .then( res => {
                 const {results} = res.data
+                console.log(results)
                 setPokeDex(results)
             })
             .catch(error => setErrorResponse("Loading..."))
@@ -18,12 +19,13 @@ const Pokemon = () => {
   return (
     <fieldset>
         <legend>Pokemon.jsx</legend>
-        <button onClick={catchEmAll}>Get Pokemon</button>
-            {errorResponse ? <p>{errorResponse}</p> : null }
-            {
-                pokeDex.map((oneMon) => <p>{oneMon.name}</p>)
-            }
-        
+        <div style = {{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+            <button style = {{width: 132}} onClick={catchEmAll}>Get Pokemon</button>
+                {errorResponse ? <p>{errorResponse}</p> : null }
+                {
+                    pokeDex.map((oneMon) => <span>{oneMon.name}</span>)
+                }
+        </div>
     </fieldset>
   )
 }
