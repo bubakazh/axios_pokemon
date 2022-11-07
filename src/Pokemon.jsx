30d83bc9556df8aba0afc931f72bf60a7929b374
bubakazh <troymanansala@gmail.com>
2022-11-07 02:51:04 -0800

    added useEffect and clear button

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,11 +1,15 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Pokemon = () => {
 
     const [ pokeDex, setPokeDex ] = useState([])
     const [ errorResponse, setErrorResponse] = useState("")
+
+    useEffect (() => {
+        catchEmAll()
+    }, [])
 
     const catchEmAll = () => {
         axios.get("https://pokeapi.co/api/v2/pokemon/?limit=905")
@@ -16,11 +20,18 @@ const Pokemon = () => {
             })
             .catch(error => setErrorResponse("Loading..."))
     }
+
+    const releaseEmAll = () => {
+        setPokeDex([])
+    }
   return (
     <fieldset>
         <legend>Pokemon.jsx</legend>
         <div style = {{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-            <button style = {{width: 132}} onClick={catchEmAll}>Get Pokemon</button>
+            <div style = {{ display: "flex", padding: 10}}>
+                <button style = {{width: 132, marginRight: 10}} onClick={catchEmAll}>Get Pokemon</button>
+                <button style = {{width: 132}} onClick={releaseEmAll}>Clear</button>
+            </div>
                 {errorResponse ? <p>{errorResponse}</p> : null }
                 {
                     pokeDex.map((oneMon) => <span>{oneMon.name}</span>)
